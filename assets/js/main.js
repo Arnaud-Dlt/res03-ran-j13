9ed729bc098f69c8fec0509399fffe8e9ca5706b
Arnaud-Dlt <arnauddeletre@outlook.fr>
2022-11-24 11:28:33 +0000

    Scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
 window.addEventListener("DOMContentLoaded", function(event){
-
+    let sections=document.querySelectorAll("body > main > section")
+    let position = sections.getBoundingClientRect();
+    let currentScrollPosition = document.documentElement.scrollTop;
+  
+    
+    function getNextSection()
+    {
+        if(sections.id==="red")
+        {
+            return document.getElementById("orange")
+        }
+        if(sections.id==="orange")
+        {
+            return document.getElementById("yellow")
+        }
+        if(sections.id==="yellow")
+        {
+            return document.getElementById("green")
+        }
+        if(sections.id==="green")
+        {
+            return document.getElementById("blue")
+        }
+        if(sections.id==="blue")
+        {
+            return document.getElementById("purple")
+        }
+        
+    }
 });
